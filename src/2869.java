cd2b7050a66b5d0881dbd8d8a1b2edc3f65532c8
nayoong02 <sonyangel02@naver.com>
2022-09-11 00:01:50 +0900

    Silver V_달팽이는 올라가고 싶다

---
+++ src/2869.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main_2869 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+        int day = (int)Math.ceil((double)(V - A) / (A - B)) + 1;
+
+        bw.write(day + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
