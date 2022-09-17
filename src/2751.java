770435fedddc9956eb9300244229cfaf6ab6129a
nayoong02 <sonyangel02@naver.com>
2022-09-17 23:58:27 +0900

    Silver V_수 정렬하기 2

---
+++ src/2751.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+class Main_2751{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int num = Integer.parseInt(br.readLine());
+        int arr[] = new int[num];
+
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+        for(int j = 0; j < arr.length; j++) {
+            sb.append(arr[j]).append("\n");
+        }
+
+        System.out.println(sb);
+
+    }
+}
