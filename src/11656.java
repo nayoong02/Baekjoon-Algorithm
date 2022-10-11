be72bad060a0a0364695d0374515e8034cfc3f57
nayoong02 <sonyangel02@naver.com>
2022-10-11 23:48:41 +0900

    Silver Ⅳ_접미사 배열

---
+++ src/11656.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Main_11656 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String word = scan.nextLine();
+        String[] list = new String[word.length()];
+
+        for(int i = 0; i < word.length(); i++) {
+            list[i] = word.substring(i);
+        }
+
+        Arrays.sort(list);
+
+        for(String li : list) {
+            System.out.println(li);
+        }
+
+    }
+}
