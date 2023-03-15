6e9662ba04d9fb4ff786d3b53005a1b9b07cd9bf
nayoong02 <sonyangel02@naver.com>
2023-03-16 00:00:00 +0900

    Silver V_줄세우기

---
+++ src/10431.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main_10431 {
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int P = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= P; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int[] arr = new int[20];
+            sb.append(st.nextToken()).append(" ");
+
+            for (int j = 0; j < 20; j++) {
+                arr[j] = Integer.parseInt(st.nextToken());
+            }
+
+            int cnt = 0; // 뒤로 물러난 횟수
+            for (int k = 1; k < 20; k++) {
+                for (int l = 0; l < k; l++) {
+                    if (arr[l] > arr[k]) {
+                        cnt++;
+                    }
+                }
+            }
+
+            sb.append(cnt).append("\n");
+
+        }
+
+        System.out.println(sb);
+
+    }
+}
