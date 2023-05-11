c9e3b6f22cd13251bbdb3e9193679d6cf39a866d
nayoong02 <sonyangel02@naver.com>
2023-05-12 05:00:42 +0900

    Silver Ⅳ_덱
    switch-case 문으로 코드 수정

--- src/DataStructures/10866.java
+++ src/DataStructures/10866.java
@@ -8,57 +8,65 @@ import java.util.LinkedList;
 class Main_10866 {
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder(); //정답 담을 버퍼
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-        LinkedList<Integer> deque = new LinkedList<Integer>(); //덱 생성
+        LinkedList<Integer> deque = new LinkedList<>(); //덱 생성
 
         for(int i=0; i<n; i++) {
             String[] com = br.readLine().split(" ");
 
-            if(com[0].equals("push_front")){
-                int m = Integer.parseInt(com[1]);
-                deque.addFirst(m);
-            }
-            else if(com[0].equals("push_back")) {
-                int m = Integer.parseInt(com[1]);
-                deque.addLast(m);
-            }
-            else if(com[0].equals("pop_front")) {
-                if(deque.isEmpty()) {
-                    sb.append(-1).append('\n');
-                } else {
-                    sb.append(deque.peekFirst()).append('\n');
-                    deque.pollFirst(); }
-            }
-            else if(com[0].equals("pop_back")) {
-                if(deque.isEmpty()) {
-                    sb.append(-1).append('\n');
-                } else {
-                    sb.append(deque.peekLast()).append('\n');
-                    deque.pollLast(); }
-            }
-            else if(com[0].equals("size")) {
-                sb.append(deque.size()).append('\n');
-            }
-            else if(com[0].equals("empty")) {
-                if(deque.isEmpty()) {
-                    sb.append(1).append('\n');
-                } else {
-                    sb.append(0).append('\n');
+            switch (com[0]) {
+                case "push_front" : {
+                    deque.addFirst(Integer.parseInt(com[1]));
+                    break;
                 }
-            }
-            else if(com[0].equals("front")) {
-                if(deque.isEmpty()) {
-                    sb.append(-1).append('\n');
-                } else {
-                    sb.append(deque.peekFirst()).append('\n');
+                case "push_back" : {
+                    deque.addLast(Integer.parseInt(com[1]));
+                    break;
                 }
-            }
-            else if(com[0].equals("back")) {
-                if(deque.isEmpty()) {
-                    sb.append(-1).append('\n');
-                } else {
-                    sb.append(deque.peekLast()).append('\n');
+                case "pop_front" : {
+                    if(deque.isEmpty()) {
+                        sb.append(-1).append('\n');
+                    } else {
+                        sb.append(deque.pollFirst()).append('\n');
+                    }
+                    break;
+                }
+                case "pop_back" : {
+                    if (deque.isEmpty()) {
+                        sb.append(-1).append('\n');
+                    } else {
+                        sb.append(deque.pollLast()).append('\n');
+                    }
+                    break;
+                }
+                case "size" : {
+                    sb.append(deque.size()).append('\n');
+                    break;
+                }
+                case "empty" : {
+                    if(deque.isEmpty()) {
+                        sb.append(1).append('\n');
+                    } else {
+                        sb.append(0).append('\n');
+                    }
+                    break;
+                }
+                case "front" : {
+                    if(deque.isEmpty()) {
+                        sb.append(-1).append('\n');
+                    } else {
+                        sb.append(deque.peekFirst()).append('\n');
+                    }
+                    break;
+                }
+                case "back" : {
+                    if(deque.isEmpty()) {
+                        sb.append(-1).append('\n');
+                    } else {
+                        sb.append(deque.peekLast()).append('\n');
+                    }
+                    break;
                 }
             }
         }
