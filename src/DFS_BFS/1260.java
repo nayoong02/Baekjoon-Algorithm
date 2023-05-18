4802f5a8877daa4863c0fc140e4d02b623a5780a
nayoong02 <sonyangel02@naver.com>
2023-05-19 02:55:42 +0900

    Silver Ⅱ_DFS와 BFS
    인접 행렬이 각각 DFS, BFS 연산을 위한 2개가 필요 없으므로 1개로 줄임.

--- src/DFS_BFS/1260.java
+++ src/DFS_BFS/1260.java
@@ -1,4 +1,5 @@
 package DFS_BFS; /**BFS와 DFS(인접행렬로 구현)**/
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,34 +7,35 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Main_1260{
-    static int[][] Dgraph = new int[1001][1001];
-    static int[][] Bgraph = new int[1001][1001];
-    static boolean[] Dvisited = new boolean[1001];
-    static boolean[] Bvisited = new boolean[1001];
+class Main_1260 {
+    static int[][] graph;
+    static boolean[] Dvisited;
+    static boolean[] Bvisited;
     static int n, m, v;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
         n = Integer.parseInt(st.nextToken()); //정점 개수
         m = Integer.parseInt(st.nextToken()); //간선 개수
         v = Integer.parseInt(st.nextToken()); //처음 노드
+
+        graph = new int[n + 1][n + 1];
+        Dvisited = new boolean[n + 1];
+        Bvisited = new boolean[n + 1];
 
         for(int i = 0; i < m; i++){ //그래프 매핑
             st = new StringTokenizer(br.readLine(), " ");
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            Dgraph[a][b] = Dgraph[b][a] = 1;
-            Bgraph[a][b] = Bgraph[b][a] = 1;
-
+            graph[a][b] = graph[b][a] = 1;
         }
 
         dfs(v);
         System.out.println();
         bfs(v);
-
     }
 
     public static void dfs(int v){
@@ -41,7 +43,7 @@ class Main_1260{
         System.out.print(v + " ");
 
         for(int i = 1; i <= n; i++){
-            if(Dgraph[v][i] == 1 && !Dvisited[i]){ //인접 노드 있고 아직 방문 전이라면 재귀
+            if(graph[v][i] == 1 && !Dvisited[i]){ //인접 노드 있고 아직 방문 전이라면 재귀
                 dfs(i);
             }
         }
@@ -58,7 +60,7 @@ class Main_1260{
             System.out.print(c + " ");
 
             for(int i = 1; i <= n; i++){
-                if(Bgraph[c][i] == 1 && !Bvisited[i]){ //인접 노드 있고 아직 방문 전이라면
+                if(graph[c][i] == 1 && !Bvisited[i]){ //인접 노드 있고 아직 방문 전이라면
                     Bvisited[i] = true; //방문 처리 후
                     queue.add(i); //큐 맨 뒤에 노드 추가
                 }
