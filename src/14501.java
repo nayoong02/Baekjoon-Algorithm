2c9970c57ac488550ec0f086c0b4a443ea94a549
nayoong02 <sonyangel02@naver.com>
2023-09-01 17:13:09 +0900

    Silver Ⅲ_퇴사
    DP

--- src/14501.java
+++ src/14501.java
@@ -3,40 +3,33 @@ import java.util.*;
 
 class Main_14501 {
     static int N;
-    static int[][] schedule;
-    static int result;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
-        schedule = new int[N][2];
+        int[] t = new int[N];
+        int[] p = new int[N];
 
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            schedule[i][0] = Integer.parseInt(st.nextToken()); // T
-            schedule[i][1] = Integer.parseInt(st.nextToken()); // P
+            t[i] = Integer.parseInt(st.nextToken()); // T
+            p[i] = Integer.parseInt(st.nextToken()); // P
         }
 
-        result = 0;
-        dfs(0, 0);
+        int[] dp = new int[N + 1];
 
-        System.out.println(result);
+        for (int i = 0; i < N; i++) {
+            if (i + t[i] <= N) { // 날짜 안 넘어가면
+                dp[i + t[i]] = Math.max(dp[i + t[i]], dp[i] + p[i]);
+            }
+
+            // 상담 중에도 이전날까지의 최대 비용 저장되어야 있어야하니까
+            // 오늘날까지의 최대 수익과 비교해 다음날의 결과값 중 최대값 갱신
+            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
+        }
+
+        System.out.println(dp[N]);
     }
 
-    static void dfs(int idx, int pay) {
-        if (idx >= N) { // idx 가 N 이상이면 result 최댓값 구하고 끝내기
-            result = Math.max(result, pay);
-            return;
-        }
-
-        if (idx + schedule[idx][0] <= N) { // 상담 끝낸 날이 N 이하이면 상담 끝난 날짜, 상담비 합쳐서 넘김.
-            dfs(idx + schedule[idx][0], pay + schedule[idx][1]);
-        } else { // N 넘어가면 상담 끝난 날짜만 넘겨줌.
-            dfs(idx + schedule[idx][0], pay);
-        }
-
-        dfs(idx + 1, pay); // 다음 날짜로 넘어가 마지막 날짜까지 모든 경우 탐색
-
-    }
 }
