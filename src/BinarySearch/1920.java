559d566144c2020348d98015d5b19501ac566b6d
nayoong02 <sonyangel02@naver.com>
2023-09-10 23:58:07 +0900

    Silver Ⅳ_수 찾기

---
+++ src/BinarySearch/1920.java
@@ -0,0 +1,52 @@
+package BinarySearch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Main_1920 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int el = Integer.parseInt(st.nextToken());
+            arr[i] = el;
+        }
+        Arrays.sort(arr);
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            boolean find = false;
+            int target = Integer.parseInt(st.nextToken());
+
+            // 이진 탐색 시작
+            int start = 0;
+            int end = arr.length - 1;
+
+            while (start <= end) {
+                int mid = (start + end) / 2;
+
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else if (target > arr[mid]) {
+                    start = mid + 1;
+                } else {
+                    find = true;
+                    break;
+                }
+            }
+
+            if (find) {
+                System.out.println(1);
+            } else {
+                System.out.println(0);
+            }
+        }
+    }
+}
