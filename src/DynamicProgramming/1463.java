3c8edd457a478c5d9fbfcd04d85b3e00cdf16c52
nayoong02 <sonyangel02@naver.com>
2023-12-10 01:13:06 +0900

    Silver Ⅲ_1로 만들기 (Bottom-Up 방식)

--- src/DynamicProgramming/1463.java
+++ src/DynamicProgramming/1463.java
@@ -1,43 +1,34 @@
-package DynamicProgramming;
+package DynamicProgramming; // Bottom - Up
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 class Main_1463 {
-    static Integer[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        Integer[] dp = new Integer[N + 1];
 
-        dp = new Integer[N + 1];
         dp[0] = dp[1] = 0;
 
-        System.out.print(recurr(N));
-    }
+        // dp[N - 1] + 1 vs (dp[N/2] + 1) vs (dp[N/3] + 1) 중 최솟값 갱신
+        for (int i = 2; i <= N; i++) {
+            // 1 빼주기 dp[N - 1] + 1
+            dp[i] = dp[i - 1] + 1;
 
-    static int recurr(int N) {
+            // 2로 나눠준 값 + 1 (dp[N/2] + 1)
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1); // 1 빼준 값, 2로 나눈 값 중 최솟값
+            }
 
-        if (dp[N] == null) {
-            // 6으로 나눠지는 경우
-            if (N % 6 == 0) {
-                dp[N] = Math.min(recurr(N - 1), Math.min(recurr(N / 3), recurr(N / 2))) + 1;
-            }
-            // 3으로만 나눠지는 경우
-            else if (N % 3 == 0) {
-                dp[N] = Math.min(recurr(N / 3), recurr(N - 1)) + 1;
-            }
-            // 2로만 나눠지는 경우
-            else if (N % 2 == 0) {
-                dp[N] = Math.min(recurr(N / 2), recurr(N - 1)) + 1;
-            }
-            // 2와 3으로 나누어지지 않는 경우
-            else {
-                dp[N] = recurr(N - 1) + 1;
+            // 3으로 나눠준 값 + 1 (dp[N/3] + 1)
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1); // 1 빼준 값, 2로 나눈 값, 3으로 나눈 값 중 최솟값
             }
         }
 
-        return dp[N];
+        System.out.print(dp[N]);
     }
 }
